4c1705c26929431ab61084b70ebfe99b86d433f3
Faustvii <Faustvii@users.noreply.github.com>
2025-07-15 17:53:12 +0200

    fix version script

--- minds_memories/newVersion.sh
+++ minds_memories/newVersion.sh
@@ -3,5 +3,6 @@ if [ "$#" -ne 2 ]; then
     echo "Usage: newVersion.sh <datapack version> <mc_version>"
     exit 1
 fi
-zip -r "MindsMemories v${1} (MC ${2})_rp.zip" pack.mcmeta data
-zip -r "MindsMemories v${1} (MC ${2})_dp.zip" pack.mcmeta assets
+
+(cd datapack/minds_memories/ && zip -r "../../MindsMemories v${1} (MC ${2})_dp.zip" .)
+(cd resourcepack/minds_memories/ && zip -r "../../MindsMemories v${1} (MC ${2})_rp.zip" .)
--- minds_memories/resourcepack/minds_memories/newVersion.sh
+++
@@ -1,6 +0,0 @@
-# if not two arguments, print usage
-if [ "$#" -ne 2 ]; then
-    echo "Usage: newVersion.sh <datapack version> <mc_version>"
-    exit 1
-fi
-zip -r "MindsMemories v${1} (MC ${2}).zip" pack.mcmeta assets
