961471eb4b6296640e46477650f2251061c0db5b
Faustvii <Faustvii@users.noreply.github.com>
2025-12-02 19:16:12 +0100

    updated newVersion script

--- newVersion.sh
+++ newVersion.sh
@@ -4,8 +4,8 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-(cd minds_resourcepack/ && zip -r "../MindsResources_v${1}_(MC ${2})_rp.zip" pack.mcmeta pack.png assets)
-(cd minds_advancements/ && zip -r "../MindsAdvancements_v${1}_(MC ${2})_dp.zip" pack.mcmeta data)
-(cd minds_memories/ && zip -r "../MindsMemories_v${1}_(MC ${2})_dp.zip" pack.mcmeta data)
-(cd minds_ghasts/ && zip -r "../MindsGhasts_v${1}_(MC ${2})_dp.zip" pack.mcmeta data)
-(cd deepcutting/ && zip -r "../MindsDeepcutting_v${1}_(MC ${2})_dp.zip" pack.mcmeta data)
+(cd minds_resourcepack/ && zip -r "../MindsResources_v${1}_mc_${2}_rp.zip" pack.mcmeta pack.png assets)
+(cd minds_advancements/ && zip -r "../MindsAdvancements_v${1}_mc_${2}_dp.zip" pack.mcmeta data)
+(cd minds_memories/ && zip -r "../MindsMemories_v${1}_mc_${2}_dp.zip" pack.mcmeta data)
+(cd minds_ghasts/ && zip -r "../MindsGhasts_v${1}_mc_${2}_dp.zip" pack.mcmeta data)
+(cd deepcutting/ && zip -r "../MindsDeepcutting_v${1}_mc_${2}_dp.zip" pack.mcmeta data)
